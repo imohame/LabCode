c889e89945c46533dbe37b27d7e510a77cc9ced0
ismail <ismail2005@gmail.com>
2016-07-01 14:21:20 -0400

    -fixed a function in CNmanager

--- src/tempin.f
+++ src/tempin.f
@@ -48,7 +48,7 @@ c
      1          ' ---------------------- '//,
      2 t5,'node',t20,'base-temp',t35,'mult-temp'/)
    81 format(t5,i5,t20,e12.5,t35,e12.5)
-   90 format(i5,2(e10.0),i5)
+   90 format(i5,2(e10.1),i5)
   110 format(' error reading nodal temperature',/,
      1       ' cards, probably for node #',i6)
       end
